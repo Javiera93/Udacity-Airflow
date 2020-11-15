21c2d32bb8b7d30fe88ecb0b7724d54e052cf558
Javiera93 <63258954+Javiera93@users.noreply.github.com>
2020-11-15 20:24:05 -0300

    Update data_quality.py

--- Udacity-Airflow/plugins/operators/data_quality.py
+++ Udacity-Airflow/plugins/operators/data_quality.py
@@ -25,4 +25,12 @@ class DataQualityOperator(BaseOperator):
         self.expected_result = expected_result
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        # self.log.info('DataQualityOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records(f"{self.test_query};")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.test_query} returned no results")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Data quality check failed. {self.test_query} contained 0 rows")
+        self.log.info(f"Data quality on table {self.test_query} check passed with {records[0][0]} records")
